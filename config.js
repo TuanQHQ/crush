4c6235954f5b6702f5ccb274ae9370d0304091bb
TuanQHQ <114071046+TuanQHQ@users.noreply.github.com>
2022-09-21 18:53:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'c Phượng ơii!',
+    introTitle: 'C Phượng ơii !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,13 +8,12 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không,em trai à :3',
-    question:'Nếu chỉ là qua đường Em sẽ ở nơi cuối con đường để đợi c',
+    btnNo: 'Không, emtrai à :3',
+    question:'Nếu chỉ là qua đường Em sẽ ở nơi cuối con đường để đợi c <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
-    messDesc: 'Tháng sau đi chơi nha công chúa',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Hẹn chị tháng sau n nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: https://www.messenger.com/t/100006458768624 
+    messLink: https://www.messenger.com/t/100006458768624
 }
-
