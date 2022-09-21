0eb12fff06d46270f598f262967187598b3cdf5d
TuanQHQ <114071046+TuanQHQ@users.noreply.github.com>
2022-09-21 19:00:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, emtrai à :3',
+    btnNo: 'Không, em trai à :3',
     question:'Nếu chỉ là qua đường Em sẽ ở nơi cuối con đường để đợi c <3',
     btnReply: 'Gửi cho em <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
-    messDesc: 'Hẹn chị tháng sau n nhé công chúa.',
+    reply: 'Chị cho e 1 cơ hội đc ko ',
+    mess: 'Em biết mà:). Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Hẹn chị tháng sau nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.messenger.com/t/100006458768624'
 }
