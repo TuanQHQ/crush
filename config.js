5b6c04abe93a5e3d0f38b3994d07ffc19e1d4f32
TuanQHQ <114071046+TuanQHQ@users.noreply.github.com>
2022-09-21 18:36:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'c Phượng à!',
+    introTitle: 'c Phượng ơii!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -9,10 +9,10 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích em <33',
     btnNo: 'Không,em trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêuchị <3',
-    btnReply: 'Gửi cho anh <3',
+    question:'Nếu chỉ là qua đường Em sẽ ở nơi cuối con đường để đợi c',
+    btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
     messDesc: 'Tháng sau đi chơi nha công chúa',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/bill.beo.96/'.
