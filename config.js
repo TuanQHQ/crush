0371db875f833ad1a12b559f27ef5b3195f391ff
TuanQHQ <114071046+TuanQHQ@users.noreply.github.com>
2022-09-21 18:24:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
-const CONFIG = {
-    introTitle: 'C Phuợng ơi!',
-    introDesc: `Nếu chỉ là qua đường
-    Em sẽ ở nơi cuối con đường để đợi c`,
+onst CONFIG = {
+    introTitle: 'c Phượng à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, em trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu chị <3',
-    btnReply: 'Gửi cho chị <3',
-    reply: 'Nắng hoàng hôn ai tô màu đỏ,Thương chị rồi co bỏ đc đâu',
-    mess: 'em 🥰. Yêu chị nhiều nhiều 😘😘',
-    messDesc: 'Hẹn chị tháng sau nhé công chúa.',
+    btnNo: 'Không,em trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêuchị <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tháng sau đi chơi nha công chúa',
     btnAccept: 'Okiiiii lun <3',
-    messLink:  'https://www.messenger.com/t/100006458768624'
-}
+    messLink: 'https://www.messenger.com/t/100002861780475/'
