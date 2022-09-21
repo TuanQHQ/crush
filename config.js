b4c6ed5f6686e0f1d987c17709e955e58c6ab548
TuanQHQ <114071046+TuanQHQ@users.noreply.github.com>
2022-09-21 18:16:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích chị <33',
-    btnNo: 'Không, chị GÁI à :3',
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'Không, em trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu chị <3',
     btnReply: 'Gửi cho chị <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Chị biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h em qua đón nhé công chúa.',
+    reply: 'Nắng hoàng hôn ai tô màu đỏ,Thương chị rồi co bỏ đc đâu',
+    mess: 'em 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Hẹn chị tháng sau nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink:  'https://www.messenger.com/t/100006458768624'
 }
